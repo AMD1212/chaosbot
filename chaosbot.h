8a191662767b68ad05d6902aad3b551a37d6a87e
Richard Leitner <dev@g0hl1n.net>
2015-02-09 12:26:07 +0100

    Move function prototypes to header file
    Create the main header file (chaosbot.h) and move the function
    prototypes there.
    
    Signed-off-by: Richard Leitner <dev@g0hl1n.net>

---
+++ chaosbot.h
@@ -0,0 +1,14 @@
+#ifndef __CHAOSBOT_H
+#define __CHAOSBOT_H
+
+/* Prototpyes */
+static int chaosbot_connect(void);
+
+/* Event Prototypes */
+static void event_connect(irc_session_t * session, const char * event, const char * origin, const char ** params, unsigned int count);
+static void event_numeric(irc_session_t * session, unsigned int event, const char * origin, const char ** params, unsigned int count);
+static void event_join(irc_session_t * session, const char * event, const char * origin, const char ** params, unsigned int count);
+static void event_channel(irc_session_t * session, const char * event, const char * origin, const char ** params, unsigned int count);
+static void event_privmsg(irc_session_t * session, const char * event, const char * origin, const char ** params, unsigned int count);
+
+#endif
