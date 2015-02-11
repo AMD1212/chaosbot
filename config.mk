211d60d6f6f1cd1455792fa1e7dd86de20e3966f
Fabian Nedoluha <finga@onders.org>
2015-02-11 23:48:34 +0100

    breake lines for readability

--- config.mk
+++ config.mk
@@ -12,7 +12,11 @@ LIBS = -L/usr/lib -lc -lircclient
 
 # flags
 CPPFLAGS = -D_BSD_SOURCE -D_POSIX_C_SOURCE=200809L -DVERSION=\"${VERSION}\"
-CFLAGS   = -std=c99 -pedantic -lm -W -Wno-unused-parameter -Wno-unused-function -Wno-unused-label -Wpointer-arith -Wformat -Wreturn-type -Wsign-compare -Wmultichar -Wformat-nonliteral -Winit-self -Wuninitialized -Wno-deprecated -Wformat-security -Wall -Werror -Wextra -Wno-missing-field-initializers -Os ${INCS} ${CPPFLAGS}
+CFLAGS   = -std=c99 -pedantic -lm -W -Wno-unused-parameter -Wno-unused-function \
+	-Wno-unused-label -Wpointer-arith -Wformat -Wreturn-type -Wsign-compare \
+	-Wmultichar -Wformat-nonliteral -Winit-self -Wuninitialized -Wno-deprecated \
+	-Wformat-security -Wall -Werror -Wextra -Wno-missing-field-initializers \
+	-Os ${INCS} ${CPPFLAGS}
 LDFLAGS  = -s ${LIBS}
 
 # compiler and linker
